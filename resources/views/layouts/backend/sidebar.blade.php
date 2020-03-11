d00051a7d550292e3198ce1b963f8c5821f5117e
01milan10 <kmilan604@gmail.com>
2020-03-12 01:17:59 +0545

    Some changes in sidebar

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -14,7 +14,7 @@
           <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block has-treeview">Alexander Pierce</a>
+          <a href="#" class="d-block has-treeview">{{Auth:: user()->name}}</a>
         </div>
       </div>
         <form action="#" method="get" class="sidebar-form">
